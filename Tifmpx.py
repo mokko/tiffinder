165d911d1884e66e44d10ca28c9acde406e6bcdb
Maurice Mengel <mauricemengel@gmail.com>
2021-01-16 19:36:22 +0100

    fix a few errors

--- Tifmpx.py
+++ Tifmpx.py
@@ -1,39 +1,21 @@
 """
-INPUT1: mpx with many multimediaobjekte
-INPUT2: dir path where tifs are stored, e.g. M:\MuseumPlus\Produktiv\Multimedia\AKU\Archiv_TIF
+Find and copy tif from mpx
 
-First step is to create in memory cache where we associate $dirname with file_tif_path, e.g.
-
-cache={
-    "A 110" : "M:\MuseumPlus\Produktiv\Multimedia\AKU\Archiv_TIF\SSOZ\III_Zentralasien\Expeditionsfotos\A 110.tif"
-}
-
-only *.tif and *.tiff are allowed case-insensitive
-
-TWO
-Then we loop thru mpx multimedia. For every jpg we look for a tif equivalent 
-e.g. 
-"B 1043 Kara Kodscha Unsere Wohnung pos.jpg" -> "B 1043 Kara Kodscha Unsere Wohnung pos.tif"
-
-THREE
-If equivalent found, save it as 
-    $mulId.$origFileName.tif
-
-FOUR
-If not found, write error log
-
-OTHER THOUGHTS
-- We dont want to fake multimediaobjekte this time around. Let the Stabi do that.
-- We could write this to use jpg as input or mpx as input. Without mpx we dont have the mulId.
+Loop thru mpx/multimedia[erweiterung eq 'jpg'] and look for *.jpg; 
+copy their corresponding *.tifs to target_dir
 
 USAGE (object)
     o = TifMpx (scan_dir)
     o.search_mpx (mpx_fn, [target_dir]) #if no target, report only
 
+cache_fn:   Global variable see below; caches the result of the scan_dir; delete
+            manually when necessary
+logfile:    target_dir/report.log
+
 USAGE Commandline (to do)
     Tifmpx.py -m mpx_fn, -s scan_dir, -t target_dir
-
 """
+
 import datetime
 import json
 import os
@@ -53,44 +35,62 @@ class TifMpx:
             self.cache = self._scan_dir (scan_dir)
 
     def search_mpx (self, mpx_fn, target_dir=None):
-        if target_dir is not None:
-            self._init_log (target_dir)
+        self._init_log (target_dir)
 
         tree = etree.parse(mpx_fn)
-        r = tree.xpath('/m:museumPlusExport/m:multimediaobjekt', 
-            namespaces={'m':'http://www.mpx.org/mpx'})
+        r = tree.xpath("/m:museumPlusExport/m:multimediaobjekt[m:erweiterung = 'jpg']", 
+            namespaces={"m":"http://www.mpx.org/mpx"})
 
         c = 1
         for mume in r:
             mulId = mume.get("mulId")
             dateiname = mume.findtext("m:dateiname", namespaces={'m':'http://www.mpx.org/mpx'})
             print (f"{c}:{mulId}: '{dateiname}'")
-            if dateiname in self.cache: 
+            if dateiname in self.cache.values(): 
                 if target_dir is not None:
                     self._copy_fn (target_dir, mulId, dateiname)
             else:
                 self._write_log (f"tif equivalent not found: '{dateiname}'")
             c = c + 1
+        self._close_log()
 
 ### PRIVATE ###
 
+    def _cache_by_name (self, name):
+        """ 
+        Search the cache for a name and return full path
+        
+        There could be multiple identical names; this method always returns
+        the first path.
+        """
+
+        for path, short in self.cache.items():
+            if short == name:
+                return path
+        raise ValueError("Name not found in cache") 
+        
     def _close_log (self):
         self._log.close()
 
     def _copy_fn (self, target_dir, mulId, dateiname):
         target_fn = os.path.join(target_dir, mulId+'.'+ dateiname+'.tif')
         print (f"-> {target_fn}")
+        source = self._cache_by_name (dateiname) #finds first only
         if not os.path.exists (target_fn):
             try:
-                shutil.copy (self.cache[dateiname], target_fn)
+                shutil.copy (source, target_fn)
             except:
-                self._write_log(f"Source file not found: {self.cache[dateiname]}")
-        else:
-            print ("Target exists already!")
+                self._write_log(f"Source file not found: {source}")
+        #else:
+        #    print ("Target exists already, not copied again")
 
     def _init_log (self,outdir):
-        #line buffer so everything gets written when it's written, so I can CTRL+C the program
-        self._log=open(outdir+'/report.log', mode="a", buffering=1)
+        if outdir is None:
+            self._log = open(os.devnull,"w")
+        else:
+            log_fn = os.path.join(outdir,'report.log')
+            #line buffer so I can CTRL+C the program at any time
+            self._log = open(log_fn, mode="a", buffering=1)
 
     def _scan_dir (self, scan_dir):
         cache = {}
@@ -102,23 +102,36 @@ class TifMpx:
             base = os.path.basename(abs)
             (trunk,ext) = os.path.splitext(base)
             print (f"**{trunk}")
-            cache[str(trunk)] = str(abs)
+            #a path is necessarily unique, a filename not at all
+            cache[str(abs)] = str(trunk) 
+
         with open(cache_fn, 'w') as f:
             json.dump(cache, f)
         return cache
-
 
     def _write_log (self, msg):
         self._log.write(f"[{datetime.datetime.now()}] {msg}\n")
         print (msg)
 
 
-
-
-
 if __name__ == "__main__":
-    scan_dir = "M:\MuseumPlus\Produktiv\Multimedia\AKU\Archiv_TIF\SSOZ\III_Zentralasien"
-    mpx_fn = r"Y:\Turfan-Bilder\20201116\2-MPX\levelup-sort.mpx" #unicode problem \\pk.de\spk\Daten\
-    target_dir = r"Y:\Turfan-Bilder\tif"
-    o = TifMpx (scan_dir)
-    o.search_mpx (mpx_fn, target_dir) #if no target, report only
+    #scan_dir = r"M:\MuseumPlus\Produktiv\Multimedia\AKU\Archiv_TIF\SSOZ\III_Zentralasien"
+    #mpx_fn = r"Y:\Turfan-Bilder\20201116\2-MPX\levelup-sort.mpx" #unicode problem \\pk.de\spk\Daten\
+    #target_dir = r"Y:\Turfan-Bilder\tif"
+    #o = TifMpx (scan_dir)
+    #o.search_mpx (mpx_fn, target_dir) #if no target, report only
+
+    import argparse
+    parser = argparse.ArgumentParser(description="Find and copy tif from mpx")
+    parser.add_argument("-m", "--mpx_fn", 
+        required = True,
+        help = "Location of mpx file (full path)")
+    parser.add_argument("-s", "--scan_dir", 
+        required = True,
+        help="The root from which we search recursively for *.tif|*.tiff")
+    parser.add_argument("-t", "--target_dir", 
+        default = None,
+        help="Dir where identified tifs are saved to ($mulId.originalName.tif)")
+    args = parser.parse_args()
+    o = TifMpx (args.scan_dir)
+    o.search_mpx (args.mpx_fn, args.target_dir) #if no target, report only
