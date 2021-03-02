e55d80ea97f7a166646d5131eb4bf59191d27648
Maurce Mengel <mauricemengel@gmail.com>
2021-03-02 08:39:39 +0100

    resize: shorter and cleverer

--- resize_DinA4.py
+++ resize_DinA4.py
@@ -12,19 +12,15 @@ files = Path(".").glob("*.tif")
 
 for f in files:
     im = Image.open(f)
-    w = im.size[0]
-    h = im.size[1]
-    if w > h:
-        ratio = 3508 / w
+    if im.width > im.height:
+        ratio = 3508 / im.width
     else:
-        ratio = 3508 / h
-    new_size = round(w * ratio), round(h * ratio)
+        ratio = 3508 / im.height
+    new_size = round(im.width * ratio), round(im.height * ratio)
+    print(f"{f}: ({im.width}, {im.height}) -> {new_size} {ratio}")
 
-    print(f"{f}: ({w}, {h}) -> {new_size} {ratio}")
-
-    new = Path(f"./{f.stem}.DinA4.tif")
     if ratio < 1:  # only shrink, dont inflate
+        new = Path(f"{f.stem}.DinA4.tif")
         if not new.is_file():
             print(f"\t saving {new}")
-            resized = im.resize(new_size)
-            resized.save(new)
+            resized = im.resize(new_size, resample=Image.LANCZOS).save(new)
