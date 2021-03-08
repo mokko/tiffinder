cc8804e660f52891f20b7ad5fa163a573abab302
Maurce Mengel <mauricemengel@gmail.com>
2021-03-08 08:47:06 +0100

    resize_DinA4: use thumbnail intead of resize to mitigate metadata problems

--- resize_DinA4.py
+++ resize_DinA4.py
@@ -2,25 +2,42 @@
 Shrink *.tif files proportionally so that the longest size is 3508 px.
 If longest size is already smaller than 3508 px, leave it be.
 DinA4 is 210 x 297 mm; at 300 dpi this makes 2480 x 3508 px.
-Currently, always act on the pwd.
+Currently, always act on *.tif files on pwd
 """
 
 from pathlib import Path
-from PIL import Image
+from PIL import Image, TiffTags
+#from PIL.ExifTags import TAGS
 
-files = Path(".").glob("*.tif")
+shape = "-DinA4.tif"
 
-for f in files:
+def per_file(f):
     im = Image.open(f)
+
     if im.width > im.height:
         ratio = 3508 / im.width
     else:
         ratio = 3508 / im.height
     new_size = round(im.width * ratio), round(im.height * ratio)
-    print(f"{f}: ({im.width}, {im.height}) -> {new_size} {ratio}")
 
     if ratio < 1:  # only shrink, dont inflate
-        new = Path(f"{f.stem}.DinA4.tif")
-        if not new.is_file():
-            print(f"\t saving {new}")
-            resized = im.resize(new_size, resample=Image.LANCZOS).save(new)
+        new_str = f.stem + shape
+        if not Path(new_str).is_file():
+            #print(Image.TiffTags.TAGS_V2)
+            print(f"{f}: ({im.width}, {im.height}) -> {new_size} {ratio}")
+            #pillow's resize creates new image without exif information 
+            #so we're using thumbnail, but I get error with test image
+            #https://github.com/python-pillow/Pillow/issues/5314
+            #Image.DEBUG=True
+            im.thumbnail(new_size, resample=Image.LANCZOS)
+            TiffTags.TAGS_V2[33723] = TiffTags.TagInfo("IptcNaaInfo", TiffTags.BYTE, 0)
+            print(f"\t saving {new_str}")
+            im.save(new_str)
+
+if __name__ == "__main__":
+    files = Path(".").glob("*.tif")
+    for f in files: 
+        if not f.match(f"*{shape}"):
+            #print (f"OPENING {f}")
+            per_file(f)
+
