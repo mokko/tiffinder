56a8e4e178be81af295d93bf9ae7adba0cd9982f
Maurce Mengel <mauricemengel@gmail.com>
2021-03-11 22:53:40 +0100

    tif2jg.py: blackened

--- tif2jpg.py
+++ tif2jpg.py
@@ -1,11 +1,10 @@
 """
 Convert all tif files in current directory to jpeg and shrink the jpeg to
 horizontal 720px if the original is bigger than that.
-
 """
-
 from pathlib import Path
 from PIL import Image
+
 
 def per_tif(f):
     im = Image.open(f)
@@ -18,11 +17,12 @@ def per_tif(f):
             new_size = round(im.width * ratio), round(im.height * ratio)
             print(f"{f}: ({im.width}, {im.height}) -> {new_size} {ratio}")
             im.thumbnail(new_size, resample=Image.LANCZOS)
-        rgb_im = im.convert('RGB')
+        rgb_im = im.convert("RGB")
         print(f"\t saving {new_str}")
         rgb_im.save(new_str)
     else:
         print(f"{new_str} exists already.")
+
 
 if __name__ == "__main__":
     files = Path(".").glob("*.tif")
