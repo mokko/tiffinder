3c004ceaa7e35e24f47e72b51053d194b2599007
Maurce Mengel <mauricemengel@gmail.com>
2021-03-14 17:22:19 +0100

    Tif_finder.py: blackened

--- Tif_finder.py
+++ Tif_finder.py
@@ -54,7 +54,8 @@ import json
 import logging
 import pprint
 from lxml import etree
-#import os
+
+# import os
 import shutil
 import time
 from PIL import Image
@@ -114,7 +115,7 @@ class Tif_finder:
         now = time.time()
         # print (f"DIFF:{now-cache_mtime}")
         # only if cache is older than one day
-        if now - cache_mtime > 10*3600 * 24:
+        if now - cache_mtime > 10 * 3600 * 24:
             # remove items from cache if file doesn't exist anymore
             for path in list(self.cache):
                 if not os.path.exists(path):
@@ -123,7 +124,7 @@ class Tif_finder:
             for dir in scan_dir:
                 self.scandir(dir)  # writes cache
         else:
-            print ("*Cache still young")
+            print("*Cache still young")
 
     def search(self, needle, target_dir=None):
         """Search tif cache for a single needle.
@@ -223,30 +224,27 @@ class Tif_finder:
         """ Just print the log to target_dir, don't copy anything."""
         for fn in results:
             print(fn)
-            #todo logging
+            # todo logging
 
     def preview_results(self, results, target_dir):
         """Make previews for the search results and copy those to target_dir.
-        
+
         Always uses change extension naming policy.
         """
-        
-        
-        
+
         for fn in results:
             target_fn = self._change_extension_policy(fn, target_dir, "jpg")
-            print (f"***{target_fn}")
+            print(f"***{target_fn}")
             self._preview(fn, target_fn)
-
 
     ############# PRIVATE STUFF #############
 
     def _default_policy(self, source, target_dir):
         """Returns target filename as pathlib object or None if identical
         file is already present at target_dir.
-        
+
         If non identical files with the same exists already in target_dir,
-        a numbered variant is returned. 
+        a numbered variant is returned.
             target_dir/name.suffix
             target_dir/name (1).suffix
             ...
@@ -257,26 +255,26 @@ class Tif_finder:
         suffix = Path(new).suffix
         parent = new.parent
         target_fn = Path(target_dir).joinpath(new.name)
-        #print(f"TTT:{target_fn}")
-        while target_fn.exists():  
+        # print(f"TTT:{target_fn}")
+        while target_fn.exists():
             if filecmp.cmp(source, target_fn, shallow=False):
                 print(f"identical file exists already in target_dir, no copy")
                 return None
             else:
                 target_fn = parent.joinpath(f"{new.stem} ({i}){suffix}")
             i += 1
-        #print(f"POLICY: [{i}] {new}")
+        # print(f"POLICY: [{i}] {new}")
         return target_fn
 
-    def _change_extension_policy (self, source, target_dir, new_ext):
+    def _change_extension_policy(self, source, target_dir, new_ext):
         """Like default policy just without identity check.
-        
+
         Note: if you run this command multiple times you'll get duplicates
         """
         i = 2
         source = Path(source)
         target_dir = Path(target_dir)
-        suffix = "."+new_ext
+        suffix = "." + new_ext
         target_fn = target_dir.joinpath(source.stem + suffix)
         while target_fn.exists():  # why while?
             target_fn = target_dir.joinpath(f"{source.stem} ({i}){suffix}")
@@ -297,7 +295,7 @@ class Tif_finder:
     def _simple_copy(self, source, target):
         """Copy source to target. Expects full paths."""
 
-        #print(f"{source} ->\n\t{target}")
+        # print(f"{source} ->\n\t{target}")
         if not target.is_file():  # no overwrite
             try:
                 shutil.copy2(source, target)  # copy2 preserves file info
@@ -338,24 +336,24 @@ if __name__ == "__main__":
         #new cache by scanning dir for *.tif[f]
         Tif_finder.py -c cache.json -u scan_dir
 
-        #same, but use intelligent cache update 
+        #same, but use intelligent cache update
         Tif_finder.py -c cache.json -i -u scan_dir
 
         #show cache using specified cache_fn
-        Tif_finder.py -c cache.json  -S 
+        Tif_finder.py -c cache.json  -S
 
     Search
         #Lookup individual needle, cp to pwd
-        Tif_finder.py -c cache.json -s needle       
+        Tif_finder.py -c cache.json -s needle
 
         #Lookup individual needle, cp to target_dir
-        Tif_finder.py -c cache.json -s needle -t target_dir      
+        Tif_finder.py -c cache.json -s needle -t target_dir
 
         #lookup multiple needles from xlsx file
-        Tif_finder.py -c cache.json -x excel_fn     
+        Tif_finder.py -c cache.json -x excel_fn
 
         #read mpx file, lookup all identNr in cache and copy to pwd
-        Tif_finder.py -c cache.json -m mpx_fn 
+        Tif_finder.py -c cache.json -m mpx_fn
 
     Output
         #normal search, but don't copy anything, just log what would happen
@@ -374,32 +372,69 @@ if __name__ == "__main__":
             print(f"*JUST LOG")
             self.log_results(r, args.target_dir)
         elif args.preview is not False:
-                print(f"*PREVIEW RESULTS")
-                self.preview_results(r, args.target_dir)
+            print(f"*PREVIEW RESULTS")
+            self.preview_results(r, args.target_dir)
         else:
             print(f"*COPYING RESULTS")
             self.cp_results(r, args.target_dir)
 
-    parser = argparse.ArgumentParser(description = "Tif_finder: Find *.tif[f] files by identNr")
+    parser = argparse.ArgumentParser(
+        description="Tif_finder: Find *.tif[f] files by identNr"
+    )
 
     # cache + target_dir
     parser.add_argument("-c", "--cache_fn", required=True, help="Path cache file")
-    parser.add_argument("-t", "--target_dir", default=Path("."), help="target directory to write to; defaults to current working directory")
+    parser.add_argument(
+        "-t",
+        "--target_dir",
+        default=Path("."),
+        help="target directory to write to; defaults to current working directory",
+    )
     # main command
     cmd = parser.add_mutually_exclusive_group(required=True)
-    cmd.add_argument("-n", "--new_cache", help="Write new cache, expects scan dir", nargs="?")
-    cmd.add_argument("-u", "--update_cache", help="Update existing cache, expects scan dir", nargs="?")
-    cmd.add_argument("-S", "--show_cache", action="store_true", help="Show cache; no parameter")
-    cmd.add_argument("-s", "--search", help="Search for a single needle", nargs="?") # needle
-    cmd.add_argument("-x", "--search_xlsx", help="Search for all needles in first column of first sheet", nargs="?")
-    cmd.add_argument("-m", "--search_mpx", help="Search for all needles in mpx/identNr", nargs="?")
+    cmd.add_argument(
+        "-n", "--new_cache", help="Write new cache, expects scan dir", nargs="?"
+    )
+    cmd.add_argument(
+        "-u",
+        "--update_cache",
+        help="Update existing cache, expects scan dir",
+        nargs="?",
+    )
+    cmd.add_argument(
+        "-S", "--show_cache", action="store_true", help="Show cache; no parameter"
+    )
+    cmd.add_argument(
+        "-s", "--search", help="Search for a single needle", nargs="?"
+    )  # needle
+    cmd.add_argument(
+        "-x",
+        "--search_xlsx",
+        help="Search for all needles in first column of first sheet",
+        nargs="?",
+    )
+    cmd.add_argument(
+        "-m", "--search_mpx", help="Search for all needles in mpx/identNr", nargs="?"
+    )
 
     # modify output
     output = parser.add_mutually_exclusive_group()
-    output.add_argument("-o", "--output", action="store_true", help="Modify output (preview, log)") 
-    output.add_argument("-p", "--preview", action="store_true", help="Don't copy tif, but make a smaller preview instead") 
-    output.add_argument("-j", "--justlog", action="store_true", help="Don't write any image files, just log") 
-    
+    output.add_argument(
+        "-o", "--output", action="store_true", help="Modify output (preview, log)"
+    )
+    output.add_argument(
+        "-p",
+        "--preview",
+        action="store_true",
+        help="Don't copy tif, but make a smaller preview instead",
+    )
+    output.add_argument(
+        "-j",
+        "--justlog",
+        action="store_true",
+        help="Don't write any image files, just log",
+    )
+
     args = parser.parse_args()
     print(args)
     print(f"*Loading specified cache '{args.cache_fn}'")
@@ -411,7 +446,7 @@ if __name__ == "__main__":
     elif args.update_cache is not None:
         t.iscandir(args.update_cache)
     elif args.show_cache is not False:
-         t.show_cache()
+        t.show_cache()
     elif args.search is not None:
         print(f"*Searching for '{args.search}'")
         r = t.search(args.search)
